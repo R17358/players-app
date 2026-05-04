51b8e2395f920ebc24019d362ea2f1ccb1927dc0
unknown <shivay.lfc@gmail.com>
2026-05-04 15:22:08 +0530

    fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,6 +27,10 @@ const reviewRoutes = require('./routes/reviewRoutes');
 const teamRoutes   = require('./routes/teamRoutes');
 const chatRoutes   = require('./routes/chatRoutes');
 
+
+// const dns = require("dns");
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 // Connect to MongoDB
 connectDB();
 
